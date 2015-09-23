2d1351ee0dbb3068056feeb2bd97c5b582a44c0c
Jorin Weatherston <jorinw@ecs354-11.csc.uvic.ca>
2015-09-22 19:33:20 -0700

    Completed first version of the u,v calulations

---
+++ raytrace-build/CMakeFiles/raytrace.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytrace.dir/main.o"
+  "CMakeFiles/raytrace.dir/sphere.o"
+  "raytrace.pdb"
+  "raytrace"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- raytrace/main.cpp
+++ raytrace/main.cpp
@@ -14,7 +14,7 @@ Colour black() { return Colour(0, 0, 0); }
 struct MyImage{
     /// Data (not private for convenience)
     int cols = 640;
-    int rows = 480;
+    int rows = 640;
     ///  Channel with [0..255] range image (aka uchar)
     cv::Mat image = cv::Mat(rows, cols, CV_8UC3, cv::Scalar(255,255,255));
 
@@ -59,7 +59,52 @@ int main(int, char**){
     ///pixel iteration
     for (int row = 0; row < image.rows; ++row) {
         for (int col = 0; col < image.cols; ++col) {
-            ///create the pixel rays in 3d spaceß
+            ///Create the Pixel Rays in 3D space
+            ///Model of Pixels from Graphical World:
+            ///The image plane in the 3D space is like a photograph in the real world, it has no segmentation, only a perfect representation of
+            /// the world it comes from. When we want to view the rendered world, we have to take this image plane and segment it into
+            /// pixels that our screen can understand. So, this means that the size of the image, and the number of pixels are related but not
+            /// equal.
+            /// We use the following formula to divide up the image into pixels in both vertical and horizontal directions
+            ///
+            ///     u = l + (r-l)(i+0.5)/nx
+            ///     v = b + (t-b)(j+0.5)/ny
+            /// , where:
+            /// - (u,v) are the coordinates of the pixel's position on the image plane w.r.t the origin,
+            /// - (l,r) and (b,t) are the distances from the origin to the left, right, bottom, and top of the image plane (this can be replaced
+            ///     with just a length and width),
+            /// - (i,j) are the iterators for which pixel you are on
+            /// - (0.5) ensures that you place the pixel into the center of the space on the image plane it belongs to
+            /// - and (nx,ny) are the number of pixels horizontally and vertically respectively.
+            ///
+            /// I will be using:
+            /// - (l,r) and (b,t) as (-a,a) and (-a,a) so that when my viewing point is located at (0,0,-1) I will have 90 degrees
+            /// of view horizontally and vertically (the zoom of the image will be decided by what a is and the distance of the camera to the
+            /// image plane),
+            /// - and (nx,ny) as equal to get a square view point, and so that there aren't any scaling issues between length and width
+            /// of the image plane and the number of pixels in the view. These are defined as image.col, image.row.
+            //left side of image plane
+            int l = -1;
+            //right side of image plane
+            int r = 1;
+            //bottom of the image plane
+            int b = -1;
+            //top of the image plane
+            int t = 1;
+            //pixel horizontal iterator (because adding 0.5 to col directly causes an infinit loop)
+            double i = col;
+            //pixel vertical iterator (because adding 0.5 to row directly causes an infinit loop)
+            double j = row;
+            //horizontal coordinates of pixel on image plane
+            double u = l + ((r - l)*(i + 0.5))/image.cols;
+            //vertical coordinates of pixel on image plane
+            double v = b + ((r - l)*(j + 0.5))/image.rows;
+            ///We now have the corrdinates of the pixel's position on the image plane w.r.t the origin (since we placed the origin at the center
+            /// of the image plane.)
+            if(row < 1){
+                cout<< "u : " << u << ",v : " << v << endl;
+            }
+
             ///create the view rays based on the pixel ray
             ///determine if the view ray intersects with the sphere (for each shape, choose closest)
                 ///if it doesn't make it black
@@ -67,10 +112,11 @@ int main(int, char**){
                     ///create ambient light color
                     ///create diffuse light
                     ///create specular lights
-            vec3 d = vec3(1,1,0).normalized();
-            ray3 r(o,d);
+            //vec3 d = vec3(1,1,0).normalized();
+            //ray3 r(o,d);
        }
     }
+    cout<< "Done!" << endl;
     //image.show();
     //image.save("output.png"); ///< Does not work on Windows!
 
