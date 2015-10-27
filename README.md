7f004b96f189fad7e386af52f35cd27bd43daf63
Jorin Weatherston <jorinw@ecs354-09.csc.uvic.ca>
2015-10-27 15:09:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Compile Instructions
+The class lab (ECS 354) is already fully setup for all your lab/homeworks:
+- https://github.com/luckysama/icg/wiki/Compile-ECS354 
+
+Although we do not officially support this, we will not prevent you from using your own laptop.
+Nonetheless, we do discourage it, _especially_ in the beginning, as it might take too much of your time:
+- https://github.com/luckysama/icg/wiki/Compile-Mac
+- https://github.com/luckysama/icg/wiki/Compile-Linux
+- https://github.com/luckysama/icg/wiki/Compile-Windows
+
+**note:** ~~currently windows has some issues with OpenCV.~~ SOLVED!
+
+# Compile Problems?
+You can use the github issue pane to report/discuss problems compiling:
+- https://github.com/luckysama/icg/issues
+
--- rasterizeline/main.cpp
+++ rasterizeline/main.cpp
@@ -1,6 +1,8 @@
 #include "icg_common.h"
 #include <Eigen/Geometry>
 
+#include <cmath>
+
 #ifndef WITH_OPENCV
     #error OpenCV required for this exercise
 #endif
@@ -9,6 +11,11 @@ typedef cv::Vec3b Colour;
 Colour red() { return Colour(255, 0, 0); }
 Colour white() { return Colour(255, 255, 255); }
 Colour black() { return Colour(0, 0, 0); }
+
+/// Rays and vectors represented with Eigen
+typedef Eigen::Vector3f vec3;
+typedef Eigen::Vector2f vec2;
+typedef Eigen::ParametrizedLine<float, 3> ray3;
 
 struct MyImage{
     /// Data (not private for convenience)
@@ -35,32 +42,32 @@ struct MyImage{
     void save(const std::string& filename){
         cv::imwrite(filename, image);
     }
+
+    void ImageCordBound(int & xCord, int & yCord)
+    {
+        if (xCord < 0) xCord = 0;
+        if (xCord >= cols) xCord = cols - 1;
+        if (yCord < 0) yCord = 0;
+        if (yCord >= rows) yCord = rows -1;
+    }
 };
 
+float LineEquation(const vec2 & p0, const vec2 & p1, float x, float y)
+{
+    //compute the implicit line equation -- four terms
+    //(y0 - y1) x + (x1 - x0) y + x0y1 -x1y0
+}
 
 int main(int, char**){
-    /// Rays and vectors represented with Eigen
-    typedef Eigen::Vector3f vec3;
-    typedef Eigen::ParametrizedLine<float, 3> ray3;
-    
+
     MyImage image;
     
-    /// TODO: define camera position and sphere position here
-
-    for (int row = 0; row < image.rows; ++row) {
-        for (int col = 0; col < image.cols; ++col) {
-            /// TODO: build primary rays
-            // vec3 o = vec3(0,0,0);
-            // vec3 d = vec3(1,1,0).normalized();
-            // ray3 r(o,d);
-            
-            /// EXAMPLE: using "image(row,col)"
-            if(row>100 && row<200 && col>200 && col<500) 
-                image(row,col) = red();
-            if(row>140 && row<240 && col>240 && col<340) 
-                image(row,col) = Colour(0,0,255);
-       }
-    }
+    //Set up parameters for the line segment 
+    vec2 lineStart(100, 200);
+    vec2 lineEnd(500, 220);   
+    //Line drawing - midpoint algorithm
+    //determine if we do a x-loop or y-loop
+    //TODO: Rasterize the line using the mid-point algorithm
     
     image.show();
     // image.save("output.png"); ///< Does not work on Windows!
